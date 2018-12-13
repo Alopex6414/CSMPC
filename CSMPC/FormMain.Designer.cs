90e8b9efd655a08a27e40a8d6f8ea7d9d8c276ae
Alopex6414 <1029535012@qq.com>
2018-12-13 16:51:25 +0800

    CSMPC Init Commit.

--- CSMPC/FormMain.Designer.cs
+++ CSMPC/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSMPC
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(944, 681);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "CSMPC";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
